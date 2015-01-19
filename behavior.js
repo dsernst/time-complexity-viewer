955a50eb766996e3a000a8ae5033a0f8ec6432f3
David Ernst <git@dsernst.com>
2015-01-19 15:18:22 -0800

    add basic profiling function

--- behavior.js
+++ behavior.js
@@ -1,3 +1,20 @@
-var input = $('#input').val();
+// var input = $('#input').val();
 
-var results = eval( '(' + input + ')').apply(null, xargs);
+var profile = function (func, input) {
+  var start = Date.now()
+  func(input)
+  return Date.now() - start
+}
+
+var sample = function(n){
+  for (var i = 0; i < n; i++) {
+    3 * 1023
+    10 + 2
+    12122124 - 12313
+    Math.floor(124 / 12534)
+  }
+}
+
+console.log(profile(sample, 100000000))
+
+// var results = eval( '(' + input + ')').apply(null, xargs);
