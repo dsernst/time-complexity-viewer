e57567efa793cc3d22902fc597c94605ff23f820
David Ernst <git@dsernst.com>
2015-01-19 17:34:43 -0800

    save work in progress on charting

--- behavior.js
+++ behavior.js
@@ -3,6 +3,7 @@ function sampleFunc (n) {
   for (var i = 0; i < n; i++) {
     a = 3 * n
     a += n > 9000
+    Infinity;
     a += 12122124 - 12313 % n
     "cat" + "dog" === n
     a += Math.floor(n / 124)
@@ -33,7 +34,37 @@ var print = function (size, time) {
   $line.append('<span class="size">' + size + '</span>: ')
   $line.append('<span class="time">' + time + '</span>ms')
   $('.results ul').append($line)
+  chart.load({
+    x: size,
+    columns: [
+      ['time', time]
+    ]
+  })
 }
+
+var chart
+
+$(function () {
+  chart = c3.generate({
+    bindto: '#chart',
+    data: {
+      columns: [
+        // ['data1', 30, 200, 100, 400, 150, 250],
+        // ['data2', 50, 20, 10, 40, 15, 25],
+        // ['size', 0,1,2,5,10,25,75,250,1000,5000,20000,100000,500000,3000000],
+      ],
+      // x: 'size'
+    },
+    axis: {
+      y: {
+        label: {
+          text: 'milliseconds (ms)',
+          position: 'outer-middle'
+        }
+      }
+    }
+  });
+})
 
 var run = function () {
   $('.results ul').text('')
