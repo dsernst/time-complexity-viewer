948ad606ceead772bb1b06fc27cba7b9d9ccf9e3
David Ernst <git@dsernst.com>
2015-01-19 16:19:12 -0800

    print results to page

--- behavior.js
+++ behavior.js
@@ -22,10 +22,18 @@ var profile = function (func, input) {
   return Date.now() - start
 }
 
+var print = function (size, time) {
+  var $line = $('<li>')
+  $line.append('<span class="size">' + size + '</span>: ')
+  $line.append('<span class="time">' + time + '</span>ms')
+  $('.results ul').append($line)
+}
+
 var run = function () {
+  $('.results ul').text('')
   var func = getInput()
   var inputSizes = [0,1,2,5,10,25,75,250,1000,5000,20000,100000,500000,3000000]
   inputSizes.forEach(function (size) {
-    console.log(size, profile(func, size))
+    print(size, profile(func, size))
   })
 }
